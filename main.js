e89aec185cf58273dfdb89877f0db90a27d8fac5
Nacamino <66182431+Nacamino@users.noreply.github.com>
2020-11-03 01:48:49 -0300

    sin retomar

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+const showBackground = () => {
+  var els = document.querySelectorAll(".number");
+  els.forEach((el, i) => {
+    if (el.id == "hueco") {
+      return null;
+    }
+    var row;
+    var col;
+    var pos_block = el.id;
+    row = Math.ceil(pos_block / 4);
+    col = pos_block - (row - 1) * 4;
+    var left = `-${(col - 1) * 100}px`;
+    var top = `-${(row - 1) * 100}px`;
+    console.log(left, top);
+    el.style.backgroundPosition = `left ${left} top ${top}`;
+  });
+};
+
+showBackground();
+
 class Tablero {
   constructor() {
     this.order = [];
@@ -5,41 +25,39 @@ class Tablero {
     this.win = false;
     this.blank_space = 15;
     this.allowedMovements = null;
-    //
+    this.gamestarted = false;
     this.cronometro = new TimeCount();
-
     this.init(true);
+    this.stop();
+    this.reanudar_game(true);
 
     this.startListeners();
-  }
+    this.sounEnabled = true;
 
-  startListeners() {
-    document.addEventListener("keydown", (event) => {
-      var allowedKeys = ["ArrowDown", "ArrowUp", "ArrowLeft", "ArrowRight"];
-      if (allowedKeys.indexOf(event.key) != -1) {
-        var key = event.key;
-        var movement = key.split("Arrow")[1].toLowerCase();
-        if (this.allowedMovements[movement] != undefined) {
-          this.changePosition(
-            this.blank_space,
-            this.allowedMovements[movement]
-          );
-        }
-      }
-    });
+    this.sound = {
+      deslizar: new Audio(`/sounds/mv.wav`),
+    };
   }
-
-  init(orden = false) {
-    this.win = false;
-    if (!orden) {
-      this.shuffle();
-      this.cronometro.start();
+  //tablero funcionando
+  shuffle() {
+    this.blank_space = 15;
+    var orden_actual = [];
+    for (let i = 1; i < 16; i++) {
+      orden_actual.push(i);
     }
 
-    this.reorderBlocks();
-    this.checkMovements();
+    var orden_nuevo = [];
+    while (orden_actual.length) {
+      let random_el = orden_actual.splice(
+        Math.floor(Math.random() * orden_actual.length),
+        1
+      );
+      orden_nuevo.push(random_el[0]);
+    }
+    orden_nuevo.push("hueco");
+    this.order = orden_nuevo;
+    console.log(this.order);
   }
-
   checkMovements() {
     var row;
     var col;
@@ -71,43 +89,44 @@ class Tablero {
 
     this.allowedMovements = movementIndexes;
     console.log(this.allowedMovements);
-    for (let arrow in allowedMovements) {
-      if (allowedMovements[arrow]) {
-        document.getElementById(arrow).style.display = "block";
-      } else {
-        document.getElementById(arrow).style.display = "none";
-      }
-    }
-  }
-
-  shuffle() {
-    this.blank_space = 15;
-    var orden_actual = [];
-    for (let i = 1; i < 16; i++) {
-      orden_actual.push(i);
-    }
-
-    var orden_nuevo = [];
-    while (orden_actual.length) {
-      let random_el = orden_actual.splice(
-        Math.floor(Math.random() * orden_actual.length),
-        1
-      );
-      orden_nuevo.push(random_el[0]);
-    }
-    orden_nuevo.push("hueco");
-    this.order = orden_nuevo;
-    console.log(this.order);
   }
 
   reorderBlocks() {
     this.order.forEach((block, i) => {
       var el = document.getElementById(block);
-      el.style.order = `${i + 1}`;
+      var row;
+      var col;
+      var pos_block = i + 1;
+      row = Math.ceil(pos_block / 4);
+      col = pos_block - (row - 1) * 4;
+
+      var left = `${(col - 1) * 25}%`;
+      var top = `${(row - 1) * 25}%`;
+      el.style.left = left;
+      el.style.top = top;
     });
   }
 
+  //inicio del juego
+  init(orden = false) {
+    this.win = false;
+
+    if (!orden) {
+      this.gamestarted = true;
+      this.shuffle();
+      this.cronometro.start();
+    } else {
+      this.order = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, "hueco"];
+    }
+
+    this.reorderBlocks();
+    this.checkMovements();
+  }
+  //movimientos
   changePosition(x, y) {
+    if (!this.gamestarted) {
+      return null;
+    }
     if (this.win) {
       return null;
     }
@@ -120,12 +139,38 @@ class Tablero {
     }
     this.order[x] = this.order[y];
     this.order[y] = temp;
+
     this.reorderBlocks();
+    this.movement_sound();
     this.checkMovements();
     this.checkWin();
     console.log(this.order);
   }
+  //controles
+  startListeners() {
+    document.addEventListener("keydown", (event) => {
+      var allowedKeys = ["ArrowDown", "ArrowUp", "ArrowLeft", "ArrowRight"];
 
+      if (allowedKeys.indexOf(event.key) != -1) {
+        var key = event.key;
+        var movement = key.split("Arrow")[1].toLowerCase();
+
+        if (this.allowedMovements[movement] != undefined) {
+          this.changePosition(
+            this.blank_space,
+            this.allowedMovements[movement]
+          );
+          //si init no se ha iniciado el sonido tampoco.
+          if (this.init(!orden)) {
+            this.startListeners();
+          }
+        }
+      }
+    });
+
+    // Setear listeners en botones de flechas
+  }
+  //fin del juego ganas o pierdes
   checkWin() {
     var ordenDeseado = [];
     for (let i = 1; i < 16; i++) {
@@ -138,9 +183,33 @@ class Tablero {
         return;
       }
     }
+    var tiempoQueToma = this.cronometro.seconds;
+    console.log(tiempoQueToma);
     this.cronometro.clean();
     this.win = true;
     alert("ganaste");
+  }
+  //sonidos
+  movement_sound() {
+    this.sounEnabled = document.getElementById("checkbox").checked;
+    if (this.sounEnabled) {
+      return;
+    }
+    this.sound.deslizar.currentTime = 0;
+    this.sound.deslizar.play();
+  }
+  //tiempo del juego
+
+  stop() {
+    clearInterval(this.cronometro.time);
+    this.gamestarted = false;
+  }
+  reanudar_game(rea = false) {
+    if (!rea) {
+      this.gamestarted = true;
+
+      this.cronometro.restart();
+    }
   }
 }
 
@@ -149,5 +218,25 @@ var tablero = new Tablero();
 document.getElementById("test").onclick = () => {
   document.getElementById("test").innerHTML = "reiniciar";
   tablero.init();
-  time_count.start();
+};
+document.getElementById("stop").onclick = () => {
+  tablero.stop();
+  if (document.getElementById("pause").classList.contains("paused--hidden")) {
+    document.getElementById("pause").classList.remove("paused--hidden");
+    document.getElementById("test").disabled = true;
+  }
+  if (!document.getElementById("pause").classList.contains("paused--hidden")) {
+    visible.classList.add("menu_game--visible");
+    document.getElementById("visible").style.display = "block";
+  }
+
+  document.getElementById("pause").onclick = () => {
+    tablero.reanudar_game();
+    if (
+      !document.getElementById("pause").classList.contains("paused--hidden")
+    ) {
+      document.getElementById("pause").classList.add("paused--hidden");
+      document.getElementById("test").disabled = false;
+    }
+  };
 };
--- prueba.js
+++ prueba.js
@@ -4,6 +4,7 @@ const addZero = (num) => {
 class TimeCount {
   constructor() {
     this.time = null;
+    this.seconds = 0;
   }
   start() {
     this.cronometer();
@@ -11,10 +12,10 @@ class TimeCount {
 
   cronometer() {
     this.clean();
-    var seconds = 0;
-    this.time = setInterval(function () {
-      seconds++;
-      var sec = seconds;
+
+    this.time = setInterval(() => {
+      this.seconds++;
+      var sec = this.seconds;
       var hour = Math.floor(sec / 3600);
       var min = Math.floor((sec % 3600) / 60);
       var secs = (sec % 3600) % 60;
@@ -26,6 +27,21 @@ class TimeCount {
   }
   clean() {
     clearInterval(this.time);
+    this.seconds = 0;
     document.getElementById("hms").innerHTML = "00:00:00";
+  }
+  restart() {
+    clearInterval(this.time);
+    this.time = setInterval(() => {
+      this.seconds++;
+      var sec = this.seconds;
+      var hour = Math.floor(sec / 3600);
+      var min = Math.floor((sec % 3600) / 60);
+      var secs = (sec % 3600) % 60;
+
+      document.getElementById("hms").innerHTML = `${addZero(hour)}:${addZero(
+        min
+      )}:${addZero(secs)}`;
+    }, 1000);
   }
 }
